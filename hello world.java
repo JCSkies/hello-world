f8ded663aeb4b274c87874c9368ee04896143884
JCSkies <69695814+JCSkies@users.noreply.github.com>
2020-08-14 13:01:34 -0700

    Add files via upload

---
+++ hello world.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Julian");
+		System.out.println("I love to study various interests");
+		System.out.println("exercise");
+		System.out.println("play video games");
+		System.out.println("and cooking!");
+		System.out.println("Also i'm not sure what I want to major in, but i'll soon find out");
+	}
+}
